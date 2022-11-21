aba3d8bd8eb7b1b7c52b24865c6e7a184fe23086
Igor <102155826+igorentiy@users.noreply.github.com>
2022-11-21 18:54:00 +0300

    added User model etc

---
+++ backend/users/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "pk",
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+    )
+    search_fields = (
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+    )
+    empty_value_display = "-пусто-"
---
+++ backend/users/models.py
@@ -0,0 +1,32 @@
+from tabnanny import verbose
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from .validators import validate_username
+
+
+class User(AbstractUser):
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+        blank=False,
+        null=False,
+        validators=(validate_username,),
+    )
+    email = models.EmailField(
+        "Электронная почта",
+        max_length=254,
+        unique=True,
+        blank=False,
+        null=False,
+    )
+    first_name = models.CharField(
+        "Имя пользователя", max_length=150, blank=True
+    )
+    last_name = models.CharField(
+        "Фамилия пользователя", max_length=150, blank=True
+    )
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "пользователи"
