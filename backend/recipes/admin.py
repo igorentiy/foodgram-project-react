77b45b5474e94f1ced013c50032dd24f134726c4
Igor <102155826+igorentiy@users.noreply.github.com>
2022-11-21 20:18:24 +0300

    modified admin-panel

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -3,22 +3,6 @@ from django.contrib import admin
 from .models import AmountIngredient, Ingredient, Recipe, Tag
 
 
-@admin.register(Recipe)
-class RecipeAdmin(admin.ModelAdmin):
-    list_display = ("name", "author", "get_in_favorites")
-    list_filter = (
-        "name",
-        "author",
-        "tags",
-    )
-    empty_value_display = "-пусто-"
-
-    def get_in_favorites(self, obj):
-        return obj.favorite_recipe.count()
-
-    get_in_favorites.short_description = "В избранных"
-
-
 @admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
     list_display = (
@@ -40,3 +24,25 @@ class TagAdmin(admin.ModelAdmin):
 @admin.register(AmountIngredient)
 class AmountIngredientAdmin(admin.ModelAdmin):
     list_display = ("recipe", "ingredient", "amount")
+
+
+class AmountIngredientInline(admin.TabularInline):
+    model = AmountIngredient
+    extra = 1
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ("name", "author", "get_in_favorites")
+    list_filter = (
+        "name",
+        "author",
+        "tags",
+    )
+    inlines = (AmountIngredientInline,)
+    empty_value_display = "-пусто-"
+
+    def get_in_favorites(self, obj):
+        return obj.favorite_recipe.count()
+
+    get_in_favorites.short_description = "В избранных"
