089b80cea02b757fb1cc4b96a8c24d558c4171aa
Igor <102155826+igorentiy@users.noreply.github.com>
2022-11-23 19:49:12 +0300

    second try

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -70,9 +70,9 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": os.getenv("DB_ENGINE", default="django.db.backends.postgresql"),
-        "NAME": os.getenv("DB_NAME", default="postgres"),
-        "USER": os.getenv("POSTGRES_USER", default="postgres"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD", default="usyhuqese"),
+        "NAME": os.getenv("DB_NAME", default="foodgram"),
+        "USER": os.getenv("POSTGRES_USER", default="foodgram_user"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", default="foodgram"),
         "HOST": os.getenv("DB_HOST", default="db"),
         "PORT": os.getenv("DB_PORT", default=5432),
     }
