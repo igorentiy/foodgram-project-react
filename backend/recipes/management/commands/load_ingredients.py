98c92593393d4f0a34555e03fc3654ad746dfc41
Igor <102155826+igorentiy@users.noreply.github.com>
2022-11-23 22:56:52 +0300

    7th try

--- backend/recipes/management/commands/load_ingredients.py
+++ backend/recipes/management/commands/load_ingredients.py
@@ -1,26 +1,23 @@
-﻿import csv
+﻿from csv import DictReader
 
 from django.conf import settings
-from django.core.management.base import BaseCommand
+from django.core.management import BaseCommand
 from recipes.models import Ingredient
 
 
 class Command(BaseCommand):
-    help = 'Загрузка ингредиентов в БД из .csv'
+    help = "Loads ingredients from /data"
 
-    def handle(self, **kwargs):
-        data_path = settings.BASE_DIR
+    def handle(self, *args, **options):
         with open(
-            f'{data_path}/data/ingredients.csv',
-            'r',
-            encoding='UTF-8'
+            f"{settings.BASE_DIR}/data/ingredients.csv",
+            "r",
+            encoding="utf-8",
         ) as file:
-            reader = csv.reader(file, delimiter=',')
-            for row in reader:
-                Ingredient.objects.get_or_create(
-                    name=row[0],
-                    measurement_unit=row[1]
-                )
-        self.stdout.write(self.style.SUCCESS(
-                          'Ингредиенты успешно загружены в БД.'
-                          ))
+            reader = DictReader(file)
+            Ingredient.objects.bulk_create(
+                Ingredient(**data) for data in reader
+            )
+        self.stdout.write(
+            self.style.SUCCESS("***Ingredients were succesfully loaded***")
+        )
